4bbd0915f2eebfff89dd09a7312c2352b3ced414
ManOfTool <a101013322@gmail.com>
2018-02-11 23:10:18 +0800

    試試

--- app.py
+++ app.py
@@ -39,15 +39,11 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(
-        event.reply_token,
-        message)
-    
-	if event.message.text == "角龍":
-		message = TextSendMessage(text="飛龍種")
+    line_bot_api.reply_message(event.reply_token, message)
+
+	if event.message.text == '角龍':
+		message = TextSendMessage(text='飛龍種')
 		line_bot_api.reply_message(event.reply_token, message)
-
-
 
 import os
 if __name__ == "__main__":
