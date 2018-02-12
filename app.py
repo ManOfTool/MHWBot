0d96665824e04cbafa7bfeb94a40c43628623ee7
ManOfTool <a101013322@gmail.com>
2018-02-12 09:51:06 +0800

    回傳圖片3

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def handle_message(event):
     elif msg == '古龍種':
         message = TextSendMessage(text='麒麟\n鋼龍\n炎王龍\n屍套龍\n滅盡龍\n熔山龍')
     elif msg == '角龍':
-        message = ImageSendMessage(original_content_url = 'https://forum.gamer.com.tw/C.php?bsn=5786&snA=137187', preview_image_url = 'https://forum.gamer.com.tw/C.php?bsn=5786&snA=137187')
+        message = ImageSendMessage(original_content_url = 'http://res.cloudinary.com/hxrp4uqty/image/upload/v1518400220/diablos.jpg', preview_image_url = 'http://res.cloudinary.com/hxrp4uqty/image/upload/v1518400220/diablos.jpg')
     else:
         message = TextSendMessage(text=msg)
 
