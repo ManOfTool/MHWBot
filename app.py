bb318c4bc3728c6effc37251ecdd76fc69f95c82
ManOfTool <a101013322@gmail.com>
2018-07-21 13:03:48 +0800

    Add Lunastra to list

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def handle_message(event):
     elif msg == '魚龍種':
         message = TextSendMessage(text='泥魚龍\n熔岩龍')
     elif msg == '古龍種':
-        message = TextSendMessage(text='麒麟\n鋼龍\n炎王龍\n屍套龍\n滅盡龍\n熔山龍\n爛輝龍')
+        message = TextSendMessage(text='麒麟\n鋼龍\n炎王龍\n炎妃龍\n屍套龍\n滅盡龍\n熔山龍\n爛輝龍')
     elif msg == '角龍':
         message = ImageSendMessage(original_content_url = 'https://res.cloudinary.com/hxrp4uqty/image/upload/v1518400220/diablos.jpg', preview_image_url = 'https://res.cloudinary.com/hxrp4uqty/image/upload/v1518400220/diablos.jpg')
     elif msg == '黑角龍':
@@ -106,6 +106,8 @@ def handle_message(event):
         message = ImageSendMessage(original_content_url = 'https://res.cloudinary.com/hxrp4uqty/image/upload/v1518403112/windy_steel.jpg', preview_image_url = 'https://res.cloudinary.com/hxrp4uqty/image/upload/v1518403112/windy_steel.jpg')
     elif msg == '炎王龍':
         message = ImageSendMessage(original_content_url = 'https://res.cloudinary.com/hxrp4uqty/image/upload/v1518401017/flame.jpg', preview_image_url = 'https://res.cloudinary.com/hxrp4uqty/image/upload/v1518401017/flame.jpg')
+    elif msg == '炎妃龍':
+        message = ImageSendMessage(original_content_url = LUNASTRA, preview_image_url = LUNASTRA)
     elif msg == '屍套龍':
         message = ImageSendMessage(original_content_url = 'https://res.cloudinary.com/hxrp4uqty/image/upload/v1518403015/walking_dead.jpg', preview_image_url = 'https://res.cloudinary.com/hxrp4uqty/image/upload/v1518403015/walking_dead.jpg')
     elif msg == '滅盡龍':
@@ -118,8 +120,6 @@ def handle_message(event):
         message = ImageSendMessage(original_content_url = 'https://res.cloudinary.com/hxrp4uqty/image/upload/v1521723041/cucumber.jpg', preview_image_url = 'https://res.cloudinary.com/hxrp4uqty/image/upload/v1521723041/cucumber.jpg')
     elif msg == '爛輝龍':
         message = ImageSendMessage(original_content_url = 'https://res.cloudinary.com/hxrp4uqty/image/upload/v1524198116/golden_sheep.jpg', preview_image_url = 'https://res.cloudinary.com/hxrp4uqty/image/upload/v1524198116/golden_sheep.jpg')
-    elif msg == '炎妃龍':
-        message = ImageSendMessage(original_content_url = LUNASTRA, preview_image_url = LUNASTRA)
 
     line_bot_api.reply_message(event.reply_token, message)
 
