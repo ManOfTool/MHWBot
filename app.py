0f160da3047ef39b3a3a4d3a9a4ab4a15905d698
ToolOfMan <a101013322@gmail.com>
2018-07-22 20:40:37 +0800

    Fix it

--- app.py
+++ app.py
@@ -119,7 +119,7 @@ def handle_message(event):
         # print(url)
 
         # message = ImageSendMessage(original_content_url = url, preview_image_url = url)
-        # message = ImageSendMessage(original_content_url = cloudinary.CloudinaryImage('mhw/LUNASTRA.jpg'), preview_image_url = LUNASTRA)
+        message = ImageSendMessage(original_content_url = cloudinary.CloudinaryImage('mhw/LUNASTRA.jpg'), preview_image_url = LUNASTRA)
     elif msg == '屍套龍':
         message = ImageSendMessage(original_content_url = 'https://res.cloudinary.com/hxrp4uqty/image/upload/v1518403015/walking_dead.jpg', preview_image_url = 'https://res.cloudinary.com/hxrp4uqty/image/upload/v1518403015/walking_dead.jpg')
     elif msg == '滅盡龍':
