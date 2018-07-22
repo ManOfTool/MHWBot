41edea23e67cb90a417013a8e91b2fc1b57c1b25
ToolOfMan <a101013322@gmail.com>
2018-07-22 12:07:55 +0800

    Add cloudinary

--- util/config.py
+++ util/config.py
@@ -1,6 +1,15 @@
 import os
+import cloudinary
+import cloudinary.api
 
 CHANNEL_ACCESS_TOKEN = os.getenv('CHANNEL_ACCESS_TOKEN', 'DEFAULT')
 CHANNEL_SECRET = os.getenv('CHANNEL_SECRET', 'DEFAULT')
+
+# cloudinary configuration
+cloudinary.config(
+	cloud_name = os.getenv('CLOUD_NAME', 'DEFAULT'),
+	api_key = os.getenv('API_KEY', 'DEFAULT'),
+	api_secret = os.getenv('API_SECRET', 'DEFAULT')
+)
 
 LUNASTRA = os.getenv('LUNASTRA', 'DEFAULT')
