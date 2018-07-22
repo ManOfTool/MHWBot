812b4ea9fb76c3381cd5332ec575ce578f21e280
ToolOfMan <a101013322@gmail.com>
2018-07-22 20:45:06 +0800

    Fix it

--- app.py
+++ app.py
@@ -114,8 +114,8 @@ def handle_message(event):
         message = ImageSendMessage(original_content_url = 'https://res.cloudinary.com/hxrp4uqty/image/upload/v1518401017/flame.jpg', preview_image_url = 'https://res.cloudinary.com/hxrp4uqty/image/upload/v1518401017/flame.jpg')
     elif msg == '炎妃龍':
         url = cloudinary.CloudinaryImage('mhw/' + monsters[msg] + '.jpg').image(secure = True)
-        url = re.findall(exp, msg)[0]
-        
+        url = re.findall(exp, msg)
+
         print(url)
 
         # message = ImageSendMessage(original_content_url = url, preview_image_url = url)
