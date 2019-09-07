172267e7416f9846587465deec6cb5a93052a2e2
MenOfTool <a101013322@gmail.com>
2019-09-07 12:00:42 +0800

    Add new monster

--- app.py
+++ app.py
@@ -43,21 +43,24 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
+    re_token = event.reply_token
 
     # Return monsters in catagory
     if msg in catagory:
         message = TextSendMessage(catagory[msg])
-
-        line_bot_api.reply_message(event.reply_token, message)
+        line_bot_api.reply_message(re_token, message)
 
     # Return informatino of monster
     elif msg in monsters:
-        url = cloudinary.CloudinaryImage(monsters[msg] + '.jpg').image(secure = True)
+        imageUrl = monsters[msg] + '.jpg'
+        if imageUrl == '.jpg':
+            imageUrl = 'mhw/TEMP.png'
+
+        url = cloudinary.CloudinaryImage(imageUrl).image(secure = True)
         url = url[10:-3]
 
         message = ImageSendMessage(original_content_url = url, preview_image_url = url)
-        
-        line_bot_api.reply_message(event.reply_token, message)
+        line_bot_api.reply_message(re_token, message)
 
     # Choose a monster randomly
     elif "$" == msg[0]:
@@ -71,8 +74,7 @@ def handle_message(event):
         shuffle(m_list)
 
         message = TextSendMessage(m_list[randrange(0, len(m_list) - 1)])
-
-        line_bot_api.reply_message(event.reply_token, message)
+        line_bot_api.reply_message(re_token, message)
 
 import os
 if __name__ == "__main__":
