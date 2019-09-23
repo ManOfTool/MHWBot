0348e45b7f549d82fe2e612b768bd60b57a934d5
ToolOfMan <a101013322@gmail.com>
2019-09-23 15:41:22 +0800

    Modified code

--- app.py
+++ app.py
@@ -21,6 +21,37 @@ line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 # Channel Secret
 handler = WebhookHandler(CHANNEL_SECRET)
 
+def msgBuild(msg):
+    message = ''
+
+    # Return monsters in catagory
+    if msg in catagory:
+        message = TextSendMessage(catagory[msg])
+
+    # Return informatino of monster
+    elif msg in monsters:
+        imageUrl = monsters[msg] + '.jpg'
+
+        url = cloudinary.CloudinaryImage(imageUrl).image(secure = True)
+        url = url[10:-3]
+
+        message = ImageSendMessage(original_content_url = url, preview_image_url = url)
+
+    # Choose a monster randomly
+    elif "$" == msg[0]:
+        cata = msg.split('$')[-1]
+
+        if cata in catagory: # choose in catagory
+            m_list = catagory[cata].split('\n')
+        else: # choose all monsters
+            m_list = list(monsters)
+
+        shuffle(m_list)
+
+        message = TextSendMessage('打' + m_list[randrange(0, len(m_list) - 1)])
+
+    return message
+
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -42,39 +73,16 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    msg = event.message.text
-    re_token = event.reply_token
+    if event.message.type == 'text':
+        msg = event.message.text
+        re_token = event.reply_token
 
-    # Return monsters in catagory
-    if msg in catagory:
-        message = TextSendMessage(catagory[msg])
-        line_bot_api.reply_message(re_token, message)
+        # No reaction to other message
+        if msg[0] == '-':
+            message = msgBuild(msg)
 
-    # Return informatino of monster
-    elif msg in monsters:
-        imageUrl = monsters[msg] + '.jpg'
-        if imageUrl == '.jpg':
-            imageUrl = 'mhw/TEMP.png'
-
-        url = cloudinary.CloudinaryImage(imageUrl).image(secure = True)
-        url = url[10:-3]
-
-        message = ImageSendMessage(original_content_url = url, preview_image_url = url)
-        line_bot_api.reply_message(re_token, message)
-
-    # Choose a monster randomly
-    elif "$" == msg[0]:
-        cata = msg.split('$')[-1]
-
-        if cata in catagory: # choose in catagory
-            m_list = catagory[cata].split('\n')
-        else: # choose all monsters
-            m_list = list(monsters)
-
-        shuffle(m_list)
-
-        message = TextSendMessage('打' + m_list[randrange(0, len(m_list) - 1)])
-        line_bot_api.reply_message(re_token, message)
+            if message != '':
+                line_bot_api.reply_message(re_token, message)
 
 import os
 if __name__ == "__main__":
